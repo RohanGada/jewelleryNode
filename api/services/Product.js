55ed963fdd308f7a9bb34e2466a472b7c86f8446
Rohan Gada <gadarohan17@gmail.com>
2017-01-25 20:18:49 +0530

    Basic Product Schema addition

--- api/services/Product.js
+++ api/services/Product.js
@@ -4,11 +4,24 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
-
-module.exports = {
-
-  attributes: {
-
+var Schema = mongoose.schema;
+var productSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
   }
-};
+});
+var attributes
+module.exports = mongoose.model('Product',productSchema);
+var models = {
 
+};
+module.exports = _.assign(module.exports,models);
