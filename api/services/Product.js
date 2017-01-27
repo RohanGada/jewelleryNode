93558cb7fbf7816265b7ddf7e755f680dd82a9ad
Rohan Gada <gadarohan17@gmail.com>
2017-01-27 11:07:33 +0530

    Schema error fix, FindOneAndUpdate set-setOnInsert dilemma tried out, more functions

--- api/services/Product.js
+++ api/services/Product.js
@@ -4,7 +4,7 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
-var Schema = mongoose.schema;
+var Schema = mongoose.Schema;
 var productSchema = new Schema({
   name: {
     type: String,
@@ -22,6 +22,62 @@ var productSchema = new Schema({
 var attributes
 module.exports = mongoose.model('Product',productSchema);
 var models = {
-
+  saveData: function (data,callback) {
+   var product = this(data);
+//    Product.findOneAndUpdate({
+//      _id: product._id
+//
+//    },{
+//      $set:data,
+//      $setOnInsert:product
+//    },{
+//      new:true,
+//      upsert:true
+//    }).exec(function (err,data) {
+//      if(err){
+//        callback(err,null);
+//      }else if(data){
+//        callback(null,data);
+//      }else{
+// // this case may never occur, 'coz in case of nil found it makes a new one. But still for safety
+//       callback(err,null);
+//      }
+//    });
+  product.save(function (err,data) {
+    if(err){
+      callback(err,null);
+    }else{
+      callback(null,data);
+    }
+  });
+},
+editData:function (data,callback) {
+  Product.findOneAndUpdate({
+    _id:data._id
+  },{
+    $set:data
+  },{
+    new:true
+  }).exec(function (err,data) {
+    if(err){
+      callback(err,null);
+    }else{
+      callback(null,data);
+    }
+  });
+},
+deleteData:function (data,callback) {
+  Product.findOneAndRemove({
+    _id:data._id
+  }).exec(function (err,data) {
+    if(err){
+      callback(err,null);
+    }else if(data){
+      callback(null,data);
+    }else{
+      callback('Document not found',null);
+    }
+  });
+}
 };
 module.exports = _.assign(module.exports,models);
