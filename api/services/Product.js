fcbe11bb8fdb312aa444091fc037c4e560823294
Rohan Gada <gadarohan17@gmail.com>
2017-02-10 23:29:42 +0530

    data type change

--- api/services/Product.js
+++ api/services/Product.js
@@ -17,67 +17,71 @@ var productSchema = new Schema({
   image: {
     type: String,
     required: true
+  },
+  status: {
+    type: Boolean,
+    default: true
   }
 });
 var attributes
-module.exports = mongoose.model('Product',productSchema);
+module.exports = mongoose.model('Product', productSchema);
 var models = {
-  saveData: function (data,callback) {
-   var product = this(data);
-//    Product.findOneAndUpdate({
-//      _id: product._id
-//
-//    },{
-//      $set:data,
-//      $setOnInsert:product
-//    },{
-//      new:true,
-//      upsert:true
-//    }).exec(function (err,data) {
-//      if(err){
-//        callback(err,null);
-//      }else if(data){
-//        callback(null,data);
-//      }else{
-// // this case may never occur, 'coz in case of nil found it makes a new one. But still for safety
-//       callback(err,null);
-//      }
-//    });
-  product.save(function (err,data) {
-    if(err){
-      callback(err,null);
-    }else{
-      callback(null,data);
-    }
-  });
-},
-editData:function (data,callback) {
-  Product.findOneAndUpdate({
-    _id:data._id
-  },{
-    $set:data
-  },{
-    new:true
-  }).exec(function (err,data) {
-    if(err){
-      callback(err,null);
-    }else{
-      callback(null,data);
-    }
-  });
-},
-deleteData:function (data,callback) {
-  Product.findOneAndRemove({
-    _id:data._id
-  }).exec(function (err,data) {
-    if(err){
-      callback(err,null);
-    }else if(data){
-      callback(null,data);
-    }else{
-      callback('Document not found',null);
-    }
-  });
-}
+  saveData: function(data, callback) {
+    var product = this(data);
+    //    Product.findOneAndUpdate({
+    //      _id: product._id
+    //
+    //    },{
+    //      $set:data,
+    //      $setOnInsert:product
+    //    },{
+    //      new:true,
+    //      upsert:true
+    //    }).exec(function (err,data) {
+    //      if(err){
+    //        callback(err,null);
+    //      }else if(data){
+    //        callback(null,data);
+    //      }else{
+    // // this case may never occur, 'coz in case of nil found it makes a new one. But still for safety
+    //       callback(err,null);
+    //      }
+    //    });
+    product.save(function(err, data) {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, data);
+      }
+    });
+  },
+  editData: function(data, callback) {
+    Product.findOneAndUpdate({
+      _id: data._id
+    }, {
+      $set: data
+    }, {
+      new: true
+    }).exec(function(err, data) {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, data);
+      }
+    });
+  },
+  deleteData: function(data, callback) {
+    Product.findOneAndRemove({
+      _id: data._id
+    }).exec(function(err, data) {
+      if (err) {
+        callback(err, null);
+      } else if (data) {
+        callback(null, data);
+      } else {
+        callback('Document not found', null);
+      }
+    });
+  }
 };
-module.exports = _.assign(module.exports,models);
+module.exports = _.assign(module.exports, models);
